ee340b429fe663256c10d1f234da35206e97477a
Georgiy <dolganov.g@gmail.com>
2022-10-21 10:04:21 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LiveJornalPostBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url="https://journaldev.com";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
